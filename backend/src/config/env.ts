8c4f2e9c2c2f484a4268fd44ff837855384d1607
Bedant Chhetri <bedantchhetri7@gmail.com>
2026-01-08 13:45:37 +0530

    add cors, clerk

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -5,5 +5,8 @@ dotenv.config()
 export const ENV = {
     PORT: process.env.PORT,
     DB_URL: process.env.DB_URL,
-    NODE_ENV: process.env.NODE_ENV
+    NODE_ENV: process.env.NODE_ENV,
+    FE_URL: process.env.FE_URL,
+    CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,14 @@
 import express from "express"
+import cors from "cors"
 import {ENV} from "./config/env"
+import { clerkMiddleware } from '@clerk/express'
+
 const app = express();
+
+app.use(cors({origin:ENV.FE_URL}))
+app.use(clerkMiddleware())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.get("/",(req,res)=>{
     res.json("hello")
